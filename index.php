9dd27703573c4221f4a39de229e0f60284b28165
Hendry Raudsepp <60486296+Timezone-design@users.noreply.github.com>
2021-05-21 07:25:54 -0700

    update htaccess

--- index.php
+++ index.php
@@ -1,10 +1,5 @@
 <?php
-$folder1 = $_GET['folder1'];
-$folder2 = $_GET['folder2'];
-$folder3 = $_GET['folder3'];
-$file = $_GET['file'];
-$cat = $_GET['cat'];
-$item = $_GET['item'];
+$uri = $_GET['uri'];
 
 // echo "$folder1/$folder2/$folder3/$file-$cat-$item";
 require(__DIR__.'/smarty/libs/Smarty.class.php');
@@ -14,29 +9,31 @@ $smarty->debugging = true;
 $smarty->caching = true;
 $smarty->cache_lifetime = 120;
 
-$js_custom_vars = array(
-	'google_conversion_id' => 970727982,
-	'google_custom_params' => 'window.google_tag_params',
-	'google_remarketing_only' => true
-);
+if($uri == '' || $uri == 'index.html'){
 
-$javascript = array(
-	'head' => array(
-		'external' => array("https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js", "https://www.kts-web.com/ajax/jquery.min.js"),
+	$js_custom_vars = array(
+		'google_conversion_id' => 970727982,
+		'google_custom_params' => 'window.google_tag_params',
+		'google_remarketing_only' => true
+	);
+
+	$javascript = array(
+		'head' => array(
+			'external' => array("https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js", "https://www.kts-web.com/ajax/jquery.min.js"),
+			'inline' => array()
+		),
+		'bottom' => array(
+			'external' => array(),
+			'inline' => array()
+		)
+	);
+
+	$stylesheets = array(
+		'external' => array("/assets/css/web_r.css","/assets/css/header_r.css","/assets/css/pc_left_menu.css","/assets/css/sp_menu.css"),
 		'inline' => array()
-	),
-	'bottom' => array(
-		'external' => array(),
-		'inline' => array()
-	)
-);
+	);
 
-$stylesheets = array(
-	'external' => array("/assets/css/web_r.css","/assets/css/header_r.css","/assets/css/pc_left_menu.css","/assets/css/sp_menu.css"),
-	'inline' => array()
-);
 
-if($file == ''){
 	$smarty->assign(array(
 		'stylesheets' => $stylesheets,
 		'javascript' => $javascript,
