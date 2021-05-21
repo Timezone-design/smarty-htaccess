990827aac1ac987dbf1eb364820b1469fbfa4e28
Hendry Raudsepp <hraudsepp10@gmail.com>
2021-05-21 05:11:59 -0700

    add smarty engine

---
+++ smarty/demo/templates_c/b854c01f5d976eacf098dff01986ca6f999fdc35_6.file.test.conf.php
@@ -0,0 +1,44 @@
+<?php /* Smarty version 3.1.39, created on 2021-05-19 15:01:28
+         compiled from 'C:\xampp\htdocs\smarty\demo\configs\test.conf' */ ?>
+<?php
+/* Smarty version 3.1.39, created on 2021-05-19 15:01:28
+  from 'C:\xampp\htdocs\smarty\demo\configs\test.conf' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.39',
+  'unifunc' => 'content_60a50c283b73f1_97708154',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'b854c01f5d976eacf098dff01986ca6f999fdc35' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\smarty\\demo\\configs\\test.conf',
+      1 => 1613599071,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_60a50c283b73f1_97708154 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->smarty->ext->configLoad->_loadConfigVars($_smarty_tpl, array (
+  'sections' => 
+  array (
+    'setup' => 
+    array (
+      'vars' => 
+      array (
+        'bold' => true,
+      ),
+    ),
+  ),
+  'vars' => 
+  array (
+    'title' => 'Welcome to Smarty!',
+    'cutoff_size' => 40,
+  ),
+));
+}
+}
