edf9c6a3506412ae01ab79431d9beee89bd5546e
Hendry Raudsepp <60486296+Timezone-design@users.noreply.github.com>
2021-05-21 05:40:08 -0700

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ $cat = $_GET['cat'];
 $item = $_GET['item'];
 
 // echo "$folder1/$folder2/$folder3/$file-$cat-$item";
-require 'smarty/libs/Smarty.class.php';
+require(__DIR__.'/smarty/libs/Smarty.class.php');
 $smarty = new Smarty;
 //$smarty->force_compile = true;
 $smarty->debugging = true;
