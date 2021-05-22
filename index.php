1f975f1645582d6b637595f25b4d9a1417e2b9db
Blu <nikitazukov1997@gmail.com>
2021-05-22 13:30:06 +0900

    css loading sequence changed

--- index.php
+++ index.php
@@ -98,11 +98,12 @@ $javascript = array(
 $stylesheets = array(
 
 	'external' => array(
+		"https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css",
+		"/assets/css/reset.min.css",
 		"/assets/css/header_blu.css",
 		"/assets/css/footer_blu.css",
 		"/assets/slick/slick.css",
 		"/assets/slick/slick-theme.css",
-		"https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css",
 		"https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap"
 	),
 	'inline' => array(
