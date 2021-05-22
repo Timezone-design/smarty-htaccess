cb53f0b1f35e7c0095e531f87273abb804f42032
Blu <nikitazukov1997@gmail.com>
2021-05-22 10:03:19 +0900

    other_blu.js created

---
+++ assets/js/other_blu.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    $('.gallery-wrapper').slick({
+        centerMode: true,
+        centerPadding: '0px',
+        slidesToShow: 3,
+        // variableWidth: true,
+        responsive: [{
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+});
