2973c812114667f4e3e287df5a603afe48d444ba
Timezonedesign <bolesalavb@gmail.com>
2021-05-25 04:43:24 +0900

    add another search function

--- modules/product_search.php
+++ modules/product_search.php
@@ -1,11 +1,11 @@
 <?php
 global $root_dir;
 $uri = $_smarty_tpl->tpl_vars['page']->value['uri'] . '#search-block';
-if ($_smarty_tpl->tpl_vars['page']->value['id_template'] == 1) {
+$id_template = $_smarty_tpl->tpl_vars['page']->value['id_template'];
+if ($id_template == 11 || $id_template == 24 || $id_template == 25) {
 	$car_manu = isset($_POST['car_manu'])?$_POST['car_manu']:'';
 	$car_model_cat = isset($_POST['car_model_cat'])?$_POST['car_model_cat']:'';
 	$product_name = isset($_POST['product_name'])?$_POST['product_name']:'';
-	$product = isset($_POST['product'])?$_POST['product']:'';
 	$file = $root_dir.'/db/car_manus.csv';
 	$handle = fopen($file, "r");
 	$car_manus = []; $car_model_cats = []; $product_names = []; $filtered_products = [];
@@ -58,7 +58,7 @@ if ($_smarty_tpl->tpl_vars['page']->value['id_template'] == 1) {
 		<?php } ?>
 	  </select>
 	  <select name="product_name">
-	  	<option value='' <?php if($product == '') echo 'selected'; ?>>製品を選ぶ</option>
+	  	<option value='' <?php if($product_name == '') echo 'selected'; ?>>製品を選ぶ</option>
 	  	<?php foreach ($product_names as $key => $value) { ?>
 	    <option value="<?=$value?>" <?php if($product_name == $value) echo 'selected'; ?>><?=$value?></option>
 		<?php } ?>
@@ -71,16 +71,16 @@ if ($_smarty_tpl->tpl_vars['page']->value['id_template'] == 1) {
 		<table class="matching_table_all">
 			<thead>
 				<tr>
-					<td>メーカー名</td>
-					<td>商品名</td>
-					<td>価格</td>
-					<td>車種</td>
-					<td>型式</td>
-					<td>年式</td>
-					<td>駆動</td>
-					<td>適合詳細</td>
-					<td>仕様</td>
-					<td>メーカー品番</td>
+					<th>メーカー名</th>
+					<th>商品名</th>
+					<th>価格</th>
+					<th>車種</th>
+					<th>型式</th>
+					<th>年式</th>
+					<th>駆動</th>
+					<th>適合詳細</th>
+					<th>仕様</th>
+					<th>メーカー品番</th>
 				</tr>
 			</thead>
 			<tbody>
@@ -104,5 +104,111 @@ if ($_smarty_tpl->tpl_vars['page']->value['id_template'] == 1) {
 	<?php } ?>
 </div>
 <?php
-}
+}else if ($id_template == 1) {
+	$inch = isset($_POST['inch'])?$_POST['inch']:'';
+	$tire_width = isset($_POST['tire_width'])?$_POST['tire_width']:'';
+	$flatness = isset($_POST['flatness'])?$_POST['flatness']:'';
+	$product = isset($_POST['product'])?$_POST['product']:'';
+	$file = $root_dir.'/db/tire.csv';
+	$handle = fopen($file, "r");
+	$inches = []; $tire_widthes = []; $flatnesses = []; $filtered_products = [];
+	$row = fgetcsv($handle, 0, ",");
+
+	while (($row = fgetcsv($handle, 0, ",")) !== false) 
+	{
+	    $product = array(
+	    	'manufacturer' => (string) $row[1],
+	    	'brand' => (string) $row[2],
+	    	'product_name' =>  $row[3],
+	    	'inch' => (string) $row[4],
+	    	'flatness' => (string) $row[5],
+	    	'tire_width' => (string) $row[6],
+	    	'price' => (string) $row[7],
+	    	'four_set' => (string) $row[8],
+	    	'note' => (string) $row[9],
+	    	'speed_notation' => (string) $row[10],
+	    	'genre' => (string) $row[11]
+	    );
+
+	    $products []= $product;
+	    if($row[4] != '') $inches[$row[4]] = $row[4];
+
+	    if($inch != '' && $product['inch'] == $inch && $product['tire_width'] != ''){
+			$tire_widthes[$product['tire_width']] = $product['tire_width'];
+			if($tire_width == $product['tire_width'] && $tire_width != ''){
+				$flatnesses[$product['flatness']] = $product['flatness'];
+				if($flatness != '' && $flatness == $product['flatness'])
+					$filtered_products []= $product;
+			}	
+		}
+					
+	}
+
+	fclose($handle);
 ?>
+<div class="search-block" id="search-block">
+	<h1 class="search">SEARCH</h1>
+	<h2>Japanese</h2>
+	<form action="/<?=$uri?>" method="post">
+	  <select name="inch" onchange="this.form.tire_width=''; this.form.flatness = ''; submit(this.form)">
+	  	<option value='' <?php if($inch == '') echo 'selected'; ?>>インチを選ぶ</option>
+	  	<?php foreach ($inches as $key => $value) { ?>
+	    <option value="<?=$value?>" <?php if($inch == $value) echo 'selected'; ?>><?=$value?></option>
+		<?php } ?>
+	  </select>
+	  <select name="tire_width" onchange="this.form.flatness = ''; submit(this.form)">
+	  	<option value='' <?php if($tire_width == '') echo 'selected'; ?> >幅を選ぶ</option>
+	    <?php foreach ($tire_widthes as $key => $value) { ?>
+	    <option value="<?=$value?>" <?php if($tire_width == $value) echo 'selected'; ?>><?=$value?></option>
+		<?php } ?>
+	  </select>
+	  <select name="flatness">
+	  	<option value='' <?php if($flatness == '') echo 'selected'; ?>>扁平率を選ぶ</option>
+	  	<?php foreach ($flatnesses as $key => $value) { ?>
+	    <option value="<?=$value?>" <?php if($flatness == $value) echo 'selected'; ?>><?=$value?></option>
+		<?php } ?>
+	  </select>
+	  <br>
+	  <button type="submit">Search</button>
+	</form>
+	<?php if(count($filtered_products) > 0){ ?>
+	<div class="search-results">	
+		<table class="matching_table_all">
+			<thead>
+				<tr>
+					<th>メーカー</th>
+					<th>ブランド</th>
+					<th>商品名</th>
+					<th>インチ</th>
+					<th>扁平率</th>
+					<th>タイヤ幅</th>
+					<th>販売価格</th>
+					<th>4本セット</th>
+					<th>備考</th>
+					<th>速度表記</th>
+					<th>ジャンル</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php foreach ($filtered_products as $key => $value) { ?>
+				<tr>
+					<td><?=$value['manufacturer']?></td>
+					<td><?=$value['brand']?></td>
+					<td><?=$value['product_name']?></td>
+					<td><?=$value['inch']?></td>
+					<td><?=$value['flatness']?></td>
+					<td><?=$value['tire_width']?></td>
+					<td><?=$value['price']?></td>
+					<td><?=$value['four_set']?></td>
+					<td><?=$value['note']?></td>
+					<td><?=$value['speed_notation']?></td>
+					<td><?=$value['genre']?></td>
+					
+				</tr>
+				<?php } ?>
+			</tbody>
+		</table>
+	</div>
+	<?php } ?>
+</div>
+<?php } ?>
